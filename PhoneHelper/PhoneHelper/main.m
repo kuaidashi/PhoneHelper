86151ec9c69b860877de4684bc6649fda640d9be
dujiepeng <347302029@qq.com>
2014-05-07 22:45:10 +0800

    create new project

---
+++ PhoneHelper/PhoneHelper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhoneHelper
+//
+//  Created by dujiepeng on 14-5-7.
+//  Copyright (c) 2014年 djp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DJPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DJPAppDelegate class]));
+    }
+}
